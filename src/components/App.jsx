94dc5cb1bb7bfbda414a78a3d9453159e57d017d
Mickael1987 <magnin.mickael@outlook.fr>
2023-04-12 16:29:56 +0200

    feat: navbar done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Header />
       <HighlightBar />
       <Welcome />
-      <ContactSection />
+      {/* <ContactSection /> */}
       <Footer />
       <NavBar />
     </div>
