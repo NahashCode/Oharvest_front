338df4702ca356ab85f2180930cb471ca6fffa01
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-04-12 23:08:02 +0200

    fix: copy logo removed + fix path logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import './NavBar.scss';
-<<<<<<< HEAD
-import logo from '../../assets/logo_oharvest.png';
-=======
->>>>>>> 8160d585f189d6ed1ada085d6f4b26583d835693
 
-import logo from '../../assets/image_cueillette.png';
+import logo from '../../assets/logo_oharvest.png';
 
 const NavBar = () => {
     return (
