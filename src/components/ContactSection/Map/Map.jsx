e45a88d3b3a16d4e0d9e9fe800f5a700cc88a200
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-04-13 17:23:35 +0200

    feat: update horaire style + add marker on map

--- src/components/ContactSection/Map/Map.jsx
+++ src/components/ContactSection/Map/Map.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import logo from '../../../assets/logo_oharvest_transparent.png';
 
 import './Map.scss';
 
 const Map = () => {
+    const position = [49.27005386352539, 3.9190235137939453];
 
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className="leaflet-map">
+        <MapContainer center={position} zoom={15} scrollWheelZoom={true} className="leaflet-map">
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <Marker position={position}>
+                <Popup>
+                    <img className="popup__logo" src={logo} />
+                </Popup>
+            </Marker>
         </MapContainer>
     );
 };
