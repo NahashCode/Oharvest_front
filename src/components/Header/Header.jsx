d677fe60064916d48caef04809ce56a9acfdf10e
Mickael1987 <magnin.mickael@outlook.fr>
2023-04-13 14:22:00 +0200

    feat: change class name and margin / padding in rem

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,12 +6,24 @@ import './Header.scss';
  */
 const Header = () => {
     return (
-        <div className='header'>
-            <img className='header__img' src='https://cdn.pixabay.com/photo/2018/07/20/14/02/grapes-3550733_960_720.jpg' alt='fruits'></img>
+        <div className="header">
+            <img
+                className="header__img"
+                src="https://cdn.pixabay.com/photo/2018/07/20/14/02/grapes-3550733_960_720.jpg"
+                alt="fruits"
+            ></img>
 
-            <img className='header__img' src='https://cdn.pixabay.com/photo/2018/11/10/11/03/pumpkin-3806393_960_720.jpg' alt='legumes'></img>
+            <img
+                className="header__img"
+                src="https://cdn.pixabay.com/photo/2018/11/10/11/03/pumpkin-3806393_960_720.jpg"
+                alt="legumes"
+            ></img>
 
-            <img className='header__img' src='https://cdn.pixabay.com/photo/2013/08/20/15/47/poppies-174276_960_720.jpg' alt='fleurs'></img>
+            <img
+                className="header__img"
+                src="https://cdn.pixabay.com/photo/2013/08/20/15/47/poppies-174276_960_720.jpg"
+                alt="fleurs"
+            ></img>
         </div>
     );
 };
