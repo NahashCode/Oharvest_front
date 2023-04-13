ddd055f4c697e8531931cbcaf1934aa52c280a16
Mickael1987 <magnin.mickael@outlook.fr>
2023-04-13 10:38:17 +0200

    caroussel done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import Welcome from './Welcome/Welcome';
 /* import ContactSection from './ContactSection/ContactSection'; */
 import NavBar from './NavBar/NavBar';
 import Footer from './Footer/Footer';
+import Caroussel from "./Caroussel/Caroussel";
 
 import './App.scss';
 
@@ -13,6 +14,7 @@ function App() {
     <div className="App">
       <Header />
       <HighlightBar />
+      <Caroussel />
       <Welcome />
       {/* <ContactSection /> */}
       <Footer />
