c798e9428f2240ab2d7bbb6baf6f6fd9691326ab
NahashCode <a.vignard@gmail.com>
2023-04-14 13:14:37 +0200

    Feat: CalendarHarvest component done + scss and box-shadow update on Banner

--- src/components/ProductPage/CalendarHarvest/CalendarHarvest.jsx
+++ src/components/ProductPage/CalendarHarvest/CalendarHarvest.jsx
@@ -6,9 +6,43 @@ import './CalendarHarvest.scss';
  */
 const CalendarHarvest = () => {
     return (
-        <div className="calendar">
-            <h2>Hello</h2>
-            
+        <div className='calendar'>
+            <table className='availability-table'>
+                <thead>
+                    <tr>
+
+                        <th>Jan</th>
+                        <th>Fév</th>
+                        <th>Mar</th>
+                        <th>Avr</th>
+                        <th>Mai</th>
+                        <th>Juin</th>
+                        <th>Juil</th>
+                        <th>Août</th>
+                        <th>Sep</th>
+                        <th>Oct</th>
+                        <th>Nov</th>
+                        <th>Déc</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+
+                        <td className='unavailable'></td>
+                        <td className='unavailable'></td>
+                        <td className='unavailable'></td>
+                        <td className='shortly'> </td>
+                        <td className='available'></td>
+                        <td className='available'></td>
+                        <td className='available'></td>
+                        <td className='available'></td>
+                        <td className='available'></td>
+                        <td className='unavailable'></td>
+                        <td className='unavailable'></td>
+                        <td className='unavailable'></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
