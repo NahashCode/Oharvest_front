a8735168511e2d365cca7533770edebdad10205a
NahashCode <a.vignard@gmail.com>
2023-04-14 14:05:58 +0200

    Feat: Variety component done + scss + responsive

--- src/components/ProductPage/Variety/Variety.jsx
+++ src/components/ProductPage/Variety/Variety.jsx
@@ -7,8 +7,8 @@ import './Variety.scss';
 const Variety = () => {
     return (
         <div className='variety'>
-            <h2>Hello world!</h2>
-            
+            <h2 className='variety__title' >Variétés</h2>
+            <p className='variety__description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non accusamus quam fugit ipsa, explicabo, magni excepturi eveniet nobis quae consequuntur amet! Suscipit, magnam veniam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam illum minus voluptas harum ipsa facilis, non recusandae mollitia velit? Fugiat, in vero eaque accusamus dolores delectus quidem sapiente asperiores autem.</p>
         </div>
     );
 };
