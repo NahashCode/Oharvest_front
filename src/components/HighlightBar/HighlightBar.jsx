da162b5d77c092f8c1a8757a9f5335b416ab5be8
Mickael1987 <magnin.mickael@outlook.fr>
2023-04-15 16:48:53 +0200

    feat: optimization of 4 files in 1

--- src/components/HighlightBar/HighlightBar.jsx
+++ src/components/HighlightBar/HighlightBar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './HighlightBar.scss';
-import { image } from './image';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-const HighlightBar = () => {
-
+const HighlightBar = (props) => {
     // Réglage du caroussel
+
+    const images = props.images || [];
 
     const settings = {
         dots: true,
@@ -23,37 +23,79 @@ const HighlightBar = () => {
                     slidesToShow: 5,
                     slidesToScroll: 2,
                     infinite: true,
-                    dots: true
-                }
+                    dots: true,
+                },
             },
             {
                 breakpoint: 425,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 2,
-                    initialSlide: 2
-                }
+                    initialSlide: 2,
+                },
             },
             {
                 breakpoint: 320,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            }
-        ]
+                    slidesToScroll: 1,
+                },
+            },
+        ],
     };
     return (
         <div>
-            <Slider {...settings}>
-                {image.map((item) => (
-                    <div key={ image.id } className="highlight">
+            {/* <Slider {...settings}>
+                {images.map((item) => (
+                    <div key={ item.id } className="highlight">
                         <div >
                             <img className="highlight__card" src={item.link} />
                         </div>
                     </div>
                 ))}
-            </Slider>
+            </Slider> */}
+            <div className='highlight'>
+                <h2 className='highlight__title'>FRUITS</h2>
+                <Slider {...settings}>
+                    {images
+                        .filter((item) => item.type === 'fruit')
+                        .map((item) => (
+                            <div key={item.id} className="highlight__div">
+                                <div>
+                                    <img className="highlight__card" src={item.link} />
+                                </div>
+                            </div>
+                        ))}
+                </Slider>
+            </div>
+            <div className='highlight'>
+                <h2 className="highlight__title">LEGUMES</h2>
+                <Slider {...settings}>
+                    {images
+                        .filter((item) => item.type === 'legume')
+                        .map((item) => (
+                            <div key={item.id} className="highlight__div">
+                                <div>
+                                    <img className="highlight__card" src={item.link} />
+                                </div>
+                            </div>
+                        ))}
+                </Slider>
+            </div>
+            <div className="highlight">
+                <h2 className="highlight__title">FLEURS</h2>
+                <Slider {...settings}>
+                    {images
+                        .filter((item) => item.type === 'fleur')
+                        .map((item) => (
+                            <div key={item.id} className="highlight__div">
+                                <div>
+                                    <img className="highlight__card" src={item.link} />
+                                </div>
+                            </div>
+                        ))}
+                </Slider>
+            </div>
         </div>
     );
 };
