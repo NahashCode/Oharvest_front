c74b7448bfec417e9cebe83d3a4ca48d259e7e31
Mickael1987 <magnin.mickael@outlook.fr>
2023-04-20 11:15:59 +0200

    feat: finalize page 404

--- src/components/404/NotFound.jsx
+++ src/components/404/NotFound.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import img from '../../assets/carrot-in-plate.png';
+import { FaHandPointRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import './NotFound.scss';
 
 const NotFound = () => {
     return (
-        <div className='notfound'>
-            <div className='notfound__div'>
+        <div className="notfound">
+            <div className="notfound__div">
                 <p className="notfound__div-title">OUPSSS...</p>
             </div>
             <section className="notfound__section">
@@ -14,6 +16,11 @@ const NotFound = () => {
                 <img className="notfound__section-img" src={img} alt="" />
                 <p className="notfound__section-p">4</p>
             </section>
+            <div className="notfound__return">
+                <Link className="notfound__return-link" to="/">
+          La Cueillette est par ici <FaHandPointRight />
+                </Link>
+            </div>
         </div>
     );
 };
