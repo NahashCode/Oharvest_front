04fd9665209f328947c1700f6bc8493ce5e680ad
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-04-22 19:56:41 +0200

    feat: add shop marker

--- src/components/CropPage/CropPlan/CropPlan.jsx
+++ src/components/CropPage/CropPlan/CropPlan.jsx
@@ -1,36 +1,29 @@
-import React, { useEffect } from 'react';
-import { MapContainer, TileLayer, LayersControl } from 'react-leaflet';
-import Plot from './Plot';
+import React from 'react';
 import PropTypes from 'prop-types';
+import {MapContainer, TileLayer, LayersControl, Marker, Popup} from 'react-leaflet';
+import Plot from './Plot';
+import UserMarker from './UserMarker';
 
 import './CropPlan.scss';
-import UserMarker from './UserMarker';
+import logo from '../../../assets/logo_oharvest_transparent.png';
 
 const CropPlan = ({data}) => {
     const position = [48.5771821, 7.7488522];
+    const shop = [48.752859, 7.414099];
 
-    useEffect(() => {
-        getUserLocation();
-    }, []);
-
-    function getUserLocation(){
-        navigator.geolocation.getCurrentPosition((position) => {
-            console.log(position.coords.latitude, position.coords.longitude);
-        });
-    }
-
-    return (
-        <MapContainer center={position} zoom={20} scrollWheelZoom={false} className="leaflet-map">
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <LayersControl position="topright">
-                {data.map(plot => <Plot key={plot.id} plot={plot} />)}
-                <UserMarker />
-            </LayersControl>
-        </MapContainer>
-    );
+    return <MapContainer center={position} zoom={15} scrollWheelZoom={false} className="leaflet-map">
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LayersControl position="topright">
+            {data.map(plot => <Plot key={plot.id} plot={plot} />)}
+            <UserMarker />
+            <Marker position={shop}>
+                <Popup><img className="popup__logo" src={logo} alt="logo O'Harvest" /></Popup>
+            </Marker>
+        </LayersControl>
+    </MapContainer>;
 };
 
 CropPlan.propTypes = {
