1ec657107d0228ed2baecd0af04eccff46598f18
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-04-23 23:38:59 +0200

    style: add text animation

--- src/components/UI/Loading/Loading.jsx
+++ src/components/UI/Loading/Loading.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 
 import './Loading.scss';
 
 const Loading = () => {
+    const [text, setText] = useState('chargement');
+
+    useEffect(() => {
+        setInterval(() => {
+            setText('chargement');
+        }, 4000);
+        setTimeout(() => {
+            setInterval(() => {
+                setText('chargement.');
+            }, 4000);
+        }, 1000);
+        setTimeout(() => {
+            setInterval(() => {
+                setText('chargement..');
+            }, 4000);
+        }, 2000);
+        setTimeout(() => {
+            setInterval(() => {
+                setText('chargement...');
+            }, 4000);
+        }, 3000);
+    }, []);
+
     return (
-        <h1 className="loading">🌀 Chargement...</h1>
+        <h1 className="loading">🌀{text}</h1>
     );
 };
 
