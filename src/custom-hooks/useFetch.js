5f70fb113977566feefdb39c1af832b3defd031d
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-04-29 14:55:15 +0200

    feat: create custom hook

---
+++ src/custom-hooks/useFetch.js
@@ -0,0 +1,34 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
+/**
+ * Custom Hook for fetching API data
+ * @param url {string}
+ * @returns {{isLoading: boolean, data: array<object>, error: object}}
+ */
+export const useFetch = (url) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasError, setHasError] = useState(false);
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        setIsLoading(true);
+        setTimeout(()=> {
+            axios.get(url)
+                .then((response) => {
+                    console.log(response.data);
+                    setData(response.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                    setHasError(true);
+                })
+                .finally(() => {
+                    setIsLoading(false);
+                });
+        }, 3000);
+    },
+    [url]);
+
+    return {data, hasError, isLoading};
+};
